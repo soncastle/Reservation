897feadab3636d7b008323ce8e2432073dd57533
soncastle <soncastle1090@gmail.com>
2025-10-21 15:08:30 +0900

    좌석배치도 추가

--- reservation/src/pages/IntroducePage.tsx
+++ reservation/src/pages/IntroducePage.tsx
@@ -5,14 +5,14 @@ import { useGoBack,  useGoMainPage } from '../hooks/useGo';
 const IntroducePage = () => {
   return (
     <div>
-        <div className='font-sans mt-2 font-semibold text-lg'>
+        <div className='font-sans mt-3 font-semibold text-lg'>
             마이리솔(My Little Sol)은 사시사철 푸른 소나무의 솔을 받아 24시, 언제든 당신을 기다립니다.
             <br/>
-            따뜻하고 맛있는 음식과 오순도순 이야기를 나누면서 영화를 볼 수 있는 마이리솔입니다.
+            따뜻하고 맛있는 음식과 함께 오순도순 이야기를 나누면서 영화를 볼 수 있는 마이리솔입니다.
         </div>
         <br/>
        <div className="overflow-hidden whitespace-nowrap">
-  <div className="inline-block animate-slide bt-2 mb-7">
+  <div className="inline-block animate-slide bt-4 mb-7">
     {Array(2).fill(0).map(() => (
       <>
         <img src="/images/4.jpg" className="inline-block w-80 h-80 mx-4 rounded-full object-cover" />
@@ -23,6 +23,9 @@ const IntroducePage = () => {
       </>
     ))}</div>
 </div>
+<hr/>
+<h3 className='mt-4 mb-3 text-4xl'>좌석 배치도</h3>
+<img src="/images/seats.png" className='w-full'/>
     </div>
   )
 }
